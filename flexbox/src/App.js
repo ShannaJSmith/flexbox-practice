d3d4b44858b91162949a259e4d82e8d1668328c7
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-02-28 12:40:00 -0500

    setup flex container and universal styling

--- flexbox/src/App.js
+++ flexbox/src/App.js
@@ -1,7 +1,11 @@
-import './App.css';
-
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <div className="flex-container">
+      <div className="item">Item A</div>
+      <div className="item">Item B</div>
+      <div className="item">Item C</div>
+    </div>
+  );
 }
 
 export default App;
