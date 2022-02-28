a6e3251c17740e3f3623cddaf23a568285a9d4d9
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-02-28 13:35:34 -0500

    create header component

---
+++ flexbox/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <heade className="header">
+      <div className="header-container">
+        <div>
+          <h1>Flexbox Sample Styling</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates
+            reprehenderit iste unde, sed molestias alias vel quaerat hic ducimus
+            placeat fugiat saepe consequuntur necessitatibus rerum praesentium.
+            Itaque recusandae sapiente magni temporibus soluta. Rem nostrum
+            distinctio magni et reiciendis animi! Ut quaerat fuga neque
+            doloremque distinctio eos nihil, ducimus dolorum vel.
+          </p>
+        </div>
+        <img src="animeDude.svg" alt="animeDude" />
+      </div>
+    </heade>
+  );
+};
+
+export default Header;
